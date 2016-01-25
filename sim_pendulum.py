b718b4d22633c17aa61981f6e1aa47fd0f55de44
Scott Hendrickson <scott@drskippy.net>
2016-01-25 06:37:45 -0700

    added configuration space plots

--- sim_pendulum.py
+++ sim_pendulum.py
@@ -1,43 +1,50 @@
 #!/usr/bin/env python
-import numpy
 import csv
 import sys
-import pandas
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+# arg 1 = w init
+# arg 2 = n periods
+# arg 3 = n ratio
 
 # time step
-dt = numpy.float64(0.05)
-# constants
-M = 1.
-L_0 = 1.
-g = 9.81
+dt = np.float64(0.00010)
 
-n = 1.5
-K = (n*n - 1) * M*g/L_0
+# constants
+L_0 = np.float64(1.0)  # unstretched length
+g = np.float64(9.81)   # gravitation
+n = np.float64(sys.argv[3])
+K_over_M = (n*n - 1)*g/L_0
 
 # initial conditions
-theta = numpy.float64(0)
-L = L_0 + M*g/K
-
-T_p = 2.*numpy.pi/numpy.sqrt(g/L)
-print "Tp = {} T/dt = {}".format(T_p, T_p/dt)
-T_k = 2.*numpy.pi/numpy.sqrt(K/M)
-print "Tk = {} T/dt = {}".format(T_k, T_k/dt)
-print "Tk/Tp = {}".format(T_k/T_p)
+theta = np.float64(0)
+L = L_0 + g/K_over_M  # equilibrium length with gravity
 
 # 2mgl = 1/2 m l^2 w^2
-w_escape = numpy.sqrt(4*g/L)
-
-w_0 = numpy.float64(sys.argv[1])
+w_sep = np.sqrt(4.*g/L)
+w_0 = np.float64(sys.argv[1])
 w = w_0
 #
 v_l_0 = 0
 v_l = v_l_0
-#
-t = numpy.float64(0.0)
-# 
-print >>sys.stderr, "w_esc = {}".format(w_escape)
 
+# periods
+T_p = 2.*np.pi/np.sqrt(g/L)
+T_k = 2.*np.pi/np.sqrt(K_over_M)
+# record some stuff
+print "Tp = {} T/dt = {}".format(T_p, T_p/dt)
+print "Tk = {} T/dt = {}".format(T_k, T_k/dt)
+print "Tk/Tp = {}".format(T_k/T_p)
+print "w_esc = {}".format(w_sep)
+
+t = np.float64(0.0)
 theta_last = theta
+
+# keep some records
 data = []
 t_s = []
 
@@ -45,11 +52,11 @@ theta += w*dt/2.
 L += v_l*dt/2.
 
 for i in range(int(sys.argv[2])*int(T_p/dt)):
-    w  += -dt*g*numpy.sin(theta)/L
-    v_l += -K*(L-L_0)/M + g*numpy.cos(theta) + w*w*L
+    w  += -dt*g*np.sin(theta)/L
+    v_l += -K_over_M*(L-L_0) + g*np.cos(theta) + w*w*L
     
     theta += w*dt
-    theta = numpy.fmod(theta, 2.*numpy.pi)
+    theta = np.fmod(theta, 2.*np.pi)
     L += v_l*dt
 
     t += dt
@@ -58,14 +65,26 @@ for i in range(int(sys.argv[2])*int(T_p/dt)):
         t_s.append(t)
     theta_last = theta
 
+# periods by measure
 t_s = [t_s[i] - t_s[i-1] for i in range(1,len(t_s)) ]
-print "avg period = {} std periods = {}".format(numpy.average(t_s), numpy.std(t_s))
-df = pandas.DataFrame().from_records(data)
+print "avg period = {} std periods = {}".format(np.average(t_s), np.std(t_s))
+
+# plots
+df = pd.DataFrame().from_records(data)
 df.columns = ["t", "theta", "omega", "l", "v_l"]
 df.set_index("t")
+
 ax = df.plot(kind="scatter", x="theta", y="omega", marker=".")
 fig = ax.get_figure()
 fig.savefig("phase1.png")
+
 ax = df.plot(kind="scatter", x="l", y="v_l", marker=".")
 fig = ax.get_figure()
 fig.savefig("phase2.png")
+
+# config space
+df["y_c"] = -df["l"]
+df["x_c"] = df["l"] * np.sin(df["theta"])
+ax = df.plot(kind="scatter", x="x_c", y="y_c", marker=".")
+fig = ax.get_figure()
+fig.savefig("config.png")
